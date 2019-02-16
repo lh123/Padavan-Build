c5f9846d496076189ac900c752bbccb6039014e5
lh123 <1585086582@qq.com>
2019-02-16 16:30:13 +0800

    clean code

--- patch.sh
+++ patch.sh
@@ -1,19 +1,13 @@
 #!/bin/sh
 SRC_DIR=/opt/rt-n56u
-echo "开始适配newifi-mini"
 if [ ! -d "${SRC_DIR}/" ] ; then
     echo "Dest dir is not exists!"
     exit 1
 fi
-
-echo "----------开始复制适配文件----------"
 if [ -d "${SRC_DIR}/trunk/configs/" ] ; then
-    cp -fRv "./newifi-mini/configs/boards/" "${SRC_DIR}/trunk/configs/"
-    cp -fRv "./newifi-mini/configs/templates/newifi-mini" "${SRC_DIR}/trunk/.config"
+    cp -frv "./newifi-mini/configs/boards/" "${SRC_DIR}/trunk/configs/"
+    cp -fv "./newifi-mini/configs/templates/newifi-mini" "${SRC_DIR}/trunk/.config"
 fi
-echo "----------复制适配文件结束----------"
-echo "----------开始复制汉化文件----------"
 if [ -d "${SRC_DIR}/trunk/user/" ] ; then
-    cp -fRv "./newifi-mini/user/" "${SRC_DIR}/trunk/"
+    cp -frv "./newifi-mini/user/" "${SRC_DIR}/trunk/"
 fi
-echo "----------复制汉化文件结束----------"
